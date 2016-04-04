70f708cbb6305c49870f236fe1e6e225933f5f16
Tyler <tyler@T-Marks-iMac-4.local>
2016-04-04 17:52:55 -0400

    Updated view and archive controller

--- client/drinks.js
+++ client/drinks.js
@@ -1,28 +1,65 @@
+angular.module('drinks', [])
 
-angular.module('drinks',[])
 
-	.controller('archive', function($scope, $http) {
-        $scope.formData = {};
+.factory('drinksFactory', ['$http',
+    function($http) {
+        return {
+            get: function() {
+                return $http.get('/api/drinks');
+            },
+            post: function() {
+                return $http.get('/api/drinks');
+            },
+            delete: function() {
+                return $http.get('/api/drinks/:drink_id');
+            },
+        }
+    }
+])
 
-        // GET =====================================================================
-        // when landing on the page, get all Drinks and show them
-        // use the service to get all the Drinks
-        $http.get('/api/drinks')
-            .success(function(data) {
-            	$scope.drinks = data;
-                $scope.whiskey = [];
-                for(drink in data){
-                	switch((data[drink].baseSpirit).toString()){
-                		case "Whiskey":
-                			$scope.whiskey.push(data[drink])
-                			break;
-                		default:
-                			console.log("No base spirit? This must be a virgin cocktail");
-                	}
+.controller('archive', ['$scope', '$http', 'drinksFactory', function($scope, $http, drinksFactory) {
 
+    $scope.formData = {};
+
+    // GET =====================================================================
+    // when landing on the page, get all Drinks and show them
+    // use the service to get all the Drinks
+    drinksFactory.get()
+        .success(function(data) {
+            $scope.drinks = data;
+            $scope.whiskey = [];
+            $scope.gin = [];
+            $scope.tequila = [];
+            $scope.vodka = [];
+            $scope.rum = [];
+            $scope.brandy = [];
+            for (drink in data) {
+                switch ((data[drink].baseSpirit).toString()) {
+                    case "Whiskey":
+                        $scope.whiskey.push(data[drink])
+                        break;
+                    case "Gin":
+                        $scope.gin.push(data[drink])
+                        break;
+                    case "Tequila":
+                        $scope.tequila.push(data[drink])
+                        break;
+                    case "Vodka":
+                        $scope.vodka.push(data[drink])
+                        break;
+                    case "Rum":
+                        $scope.rum.push(data[drink])
+                        break;
+                    case "Brandy":
+                        $scope.brandy.push(data[drink])
+                        break;
+                    default:
+                        console.log("No base spirit? This must be a virgin cocktail");
                 }
-                //console.log(data);
-            });
-        });
 
-        
+            }
+        })
+        .error(function(data){
+            console.log(data);
+        });
+}]);
