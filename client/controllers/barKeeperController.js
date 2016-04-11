40621b6f9a5609dad7c17ac4bc98838f0cde2f5f
Tyler <tyler@T-Marks-iMac-4.local>
2016-04-11 16:46:11 -0400

    Added success alerts and code comments

--- client/controllers/barKeeperController.js
+++ client/controllers/barKeeperController.js
@@ -1,6 +1,5 @@
 angular.module('barKeeper', [])
-
-
+//Factory for http calls to the node API
 .factory('barKeeperFacotry', ['$http',
     function($http) {
         return {
@@ -16,92 +15,96 @@ angular.module('barKeeper', [])
         }
     }
 ])
+//Main controller for bar keeper application
+.controller('barKeeperController', ['$scope', '$http', 'barKeeperFacotry',
+    function($scope, $http, barKeeperFacotry) {
+        //Data from form to create new drink
+        $scope.formData = {};
+        //Flag to indicate new drink was created successfully
+        $scope.createdSuccessFlag = false;
+        //Flag to indicate new drink was deleted successfully
+        $scope.deleteSuccessFlag = false;
 
-.controller('barKeeperController', ['$scope', '$http', 'barKeeperFacotry', function($scope, $http, barKeeperFacotry) {
+        //Current number of drinks for delete drink function
+        $scope.deleteDrinkCount = 0;
 
-    //Data from form to create new drink
-    $scope.formData = {};
-    $scope.createdSuccessFlag = false;
-    
-    $scope.deleteSuccessFlag = false;
-    
-    $scope.deleteDrinkCount = 0;
-    
-    $scope.createDrinkCount = 0;
-    
-    $scope.deletedDrink = "";
-   
-    $scope.createdDrink = "";
+        //Current number of drinks for create drink function
+        $scope.createDrinkCount = 0;
 
-
-    //Function to sort drinks into base spirit cat
-    function sortBaseSpirit(data) {
-        $scope.drinks = data;
-        $scope.whiskey = [];
-        $scope.gin = [];
-        $scope.tequila = [];
-        $scope.vodka = [];
-        $scope.rum = [];
-        $scope.brandy = [];
-        for (drink in data) {
-            switch ((data[drink].baseSpirit).toString()) {
-                case "Whiskey":
-                    $scope.whiskey.push(data[drink])
-                    break;
-                case "Gin":
-                    $scope.gin.push(data[drink])
-                    break;
-                case "Tequila":
-                    $scope.tequila.push(data[drink])
-                    break;
-                case "Vodka":
-                    $scope.vodka.push(data[drink])
-                    break;
-                case "Rum":
-                    $scope.rum.push(data[drink])
-                    break;
-                case "Brandy":
-                    $scope.brandy.push(data[drink])
-                    break;
-                default:
-                    console.log("No base spirit? This must be a virgin cocktail");
-            }
-
-        }
-    }
-    //CREATE==========================================================================
-    // Validate data from the form and send to Node API
-    $scope.createDrink = function() {
-        $scope.createSuccessFlag = false;
+        //Name of last deleted drink
         $scope.deletedDrink = "";
-        barKeeperFacotry.create($scope.formData)
+
+        //Name of last created drink
+        $scope.createdDrink = "";
+        //Function to sort drinks into base spirit cat
+        function sortBaseSpirit(data) {
+            $scope.drinks = data;
+            $scope.whiskey = [];
+            $scope.gin = [];
+            $scope.tequila = [];
+            $scope.vodka = [];
+            $scope.rum = [];
+            $scope.brandy = [];
+            for (drink in data) {
+                switch ((data[drink].baseSpirit).toString()) {
+                    case "Whiskey":
+                        $scope.whiskey.push(data[drink])
+                        break;
+                    case "Gin":
+                        $scope.gin.push(data[drink])
+                        break;
+                    case "Tequila":
+                        $scope.tequila.push(data[drink])
+                        break;
+                    case "Vodka":
+                        $scope.vodka.push(data[drink])
+                        break;
+                    case "Rum":
+                        $scope.rum.push(data[drink])
+                        break;
+                    case "Brandy":
+                        $scope.brandy.push(data[drink])
+                        break;
+                    default:
+                        console.log("No base spirit? This must be a virgin cocktail");
+                }
+            }
+        }
+        //CREATE==========================================================================
+        // Validate data from the form and send to Node API
+        $scope.createDrink = function() {
+            $scope.createSuccessFlag = false;
+            $scope.createdDrink = $scope.formData.name;
+            barKeeperFacotry.create($scope.formData)
+                .success(function(data) {
+                    $scope.createdDrink = $scope.formData.name;
+                    //console.log($scope.createdDrink);
+                    $scope.createDrinkCount = $scope.drinks.length;
+                    sortBaseSpirit(data);
+                    if ($scope.createDrinkCount < $scope.drinks.length) {
+                        $scope.createSuccessFlag = true;
+                        $scope.formData = {};
+                    }
+                })
+        }
+        //DELETE====================================================================
+        $scope.deleteDrink = function(id, name) {
+            $scope.deleteSuccessFlag = false;
+            $scope.deletedDrink = name.toString();
+            barKeeperFacotry.delete(id)
+                .success(function(data) {
+                    $scope.deleteDrinkCount = $scope.drinks.length;
+                    sortBaseSpirit(data)
+                    if ($scope.deleteDrinkCount > $scope.drinks.length) {
+                        $scope.deleteSuccessFlag = true;
+                    }
+                });
+        }
+        // GET =====================================================================
+        //When landing on the page, get all Drinks and show them
+        barKeeperFacotry.get()
             .success(function(data) {
-                    $scope.deleteDrinkCount = $scope.drinks.length; 
-                    $scope.formData = {}; 
-                    sortBaseSpirit(data); 
-                    console.log($scope.drinks.length);
+                sortBaseSpirit(data);
             })
     }
-    //DELETE====================================================================
-    $scope.deleteDrink = function(id, name) {
-        $scope.deleteSuccessFlag = false;
-        $scope.deletedDrink = name.toString();
-        barKeeperFacotry.delete(id)
-            .success(function(data) {
-                $scope.deleteDrinkCount = $scope.drinks.length;
-                sortBaseSpirit(data)
-                if($scope.deleteDrinkCount > $scope.drinks.length){
-                     $scope.deleteSuccessFlag = true;
-                }
-            });
-    }
-    // GET =====================================================================
-    // when landing on the page, get all Drinks and show them
-    // use the service to get all the Drinks
-    barKeeperFacotry.get()
-    .success(function(data) {
-        sortBaseSpirit(data);
-    })
-
-
-}]);
+]);
---
+++ client/js/custom.js
@@ -0,0 +1,6 @@
+$.backstretch([
+        "https://914b8c6e33652974a40fb5593f46120dbaa24a55.googledrive.com/host/0B6QFai18se5jcHhaSzZrZzNpdGM/beer-lg.jpg", "https://914b8c6e33652974a40fb5593f46120dbaa24a55.googledrive.com/host/0B6QFai18se5jcHhaSzZrZzNpdGM/martini-lg.jpg", "https://914b8c6e33652974a40fb5593f46120dbaa24a55.googledrive.com/host/0B6QFai18se5jcHhaSzZrZzNpdGM/whiskey-lg.jpg", "https://914b8c6e33652974a40fb5593f46120dbaa24a55.googledrive.com/host/0B6QFai18se5jcHhaSzZrZzNpdGM/wine-glasses-lg.jpg"
+    ], {
+        duration: 3000,
+        fade: 750
+    });
