55e6ba7185f5a31469213b106ce9ce7e00112596
Tyler Schumacher <tylerschumacher635@gmail.com>
2016-10-19 13:39:06 -0400

    Add files via upload

--- client/js/custom.js
+++ client/js/custom.js
@@ -1,5 +1,5 @@
 $.backstretch([
-        "https://914b8c6e33652974a40fb5593f46120dbaa24a55.googledrive.com/host/0B6QFai18se5jcHhaSzZrZzNpdGM/beer-lg.jpg", "https://914b8c6e33652974a40fb5593f46120dbaa24a55.googledrive.com/host/0B6QFai18se5jcHhaSzZrZzNpdGM/martini-lg.jpg", "https://914b8c6e33652974a40fb5593f46120dbaa24a55.googledrive.com/host/0B6QFai18se5jcHhaSzZrZzNpdGM/whiskey-lg.jpg", "https://914b8c6e33652974a40fb5593f46120dbaa24a55.googledrive.com/host/0B6QFai18se5jcHhaSzZrZzNpdGM/wine-glasses-lg.jpg"
+        "../images/martini-lg.jpg", "../images/beer-lg.jpg", "../images/whiskey-lg.jpg", "../images/wine-glasses-lg.jpg"
     ], {
         duration: 3000,
         fade: 750
